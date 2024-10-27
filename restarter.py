02d2e511382b2c45b0b2e3f94af57de728b4ce63
JohnLesterDev <johnlesterincbusiness@gmail.com>
2024-10-27 16:24:29 +0800

    Added Restarter for Dev

---
+++ restarter.py
@@ -0,0 +1,32 @@
+import os
+import time
+import subprocess
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+class RestartHandler(FileSystemEventHandler):
+    def __init__(self, script):
+        self.script = script
+        self.process = self.start_bot()
+
+    def start_bot(self):
+        return subprocess.Popen(["py", self.script])
+
+    def on_any_event(self, event):
+        self.process.terminate()
+        self.process = self.start_bot()
+
+if __name__ == "__main__":
+    path = "."
+    event_handler = RestartHandler("main.py")  
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
